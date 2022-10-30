31967115b1a0a7360f5bf5e9067c4ddcfad33a1b
Vincent Pauvert <pauvert.vincent@gmail.com>
2022-10-30 23:39:53 +0100

    test affichage des produits dans le panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,23 +2,24 @@
  * Récuperation des items dans le panier
  */
 let itemSaveInCart = JSON.parse(localStorage.getItem("itemInCart"));
-localStorage.getItem('itemInCart');
 /**
  * Utilisation de la boucle for in pour parcourir l'array & insertion des éléments dans la page produits
  */
-function cartArray(itemSaveInCart) {
-    for(let i of itemInCart) {
+function cartArray() {
+    for(let i in itemSaveInCart) {
         //élément article
         let article = document.getElementById('cart__items');
         article.classList.add('cart__item');
+        //article.classList.add('data-id, data-color')
 
         //élément imgContainer
         let imgContainer = document.createElement('div');
+        imgContainer.classList.add('cart__item__img')
         article.appendChild(imgContainer);
         let basketImg = document.createElement('img');
         imgContainer.appendChild(basketImg);
-        basketImg.src = itemInCart[i].image;
-        basketImg.alt = itemInCart[i].alt;
+        basketImg.src = itemSaveInCart[i].image;
+        basketImg.alt = itemSaveInCart[i].alt;
 
         //élément infoContainer
         let infoContainer = document.createElement('div');
@@ -28,6 +29,9 @@ function cartArray(itemSaveInCart) {
         infoDescription.classList.add('cart__item__content__description');
         let basketName = document.createElement('h2');
         infoDescription.appendChild(basketName);
-        basketName.textContent = itemInCart[i].nom; 
+        basketName.textContent = itemSaveInCart[i].nom;
+        let basketColor = document.createElement('p');
+        infoDescription.appendChild(basketColor);
+        basketColor.textContent = itemSaveInCart[i].color;
     }
 }
